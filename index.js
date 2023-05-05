cc4f489fa357591be395f5326b2ae939666a9647
Ayantik2002 <ayantikbhaumik@gmail.com>
2023-05-05 12:57:39 +0530

    first commit

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const cookieParser = require("cookie-parser");
 const multer = require("multer");
 const uploadMiddleware = multer({ dest: "uploads/" });
 const fs = require("fs");
-const BASE_URL= process.env.BASE_URL
 
 const secret = "afhadadafafbafbajfbajfbajfbajfbafbafbafbafjsfjkan";
 const salt = bcrypt.genSaltSync(10);
@@ -86,38 +85,6 @@ app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
     res.json(postDoc);
   });
 });
-
-// uploadMiddleware.single("file"),
-
-// app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
-//   let newPath = null;
-//   if (req.file) {
-//     const { originalname, path } = req.file;
-//     const parts = originalname.split(".");
-//     const ext = parts[parts.length - 1];
-//     newPath = path + "." + ext;
-//     fs.renameSync(path, newPath);
-//   }
-
-//   const { token } = req.cookies;
-//   jwt.verify(token, secret, {}, async (err, info) => {
-//     if (err) throw err;
-//     const { id, title, summary, content } = req.body;
-//     const postDoc = await Post.findById(id);
-//     const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
-//     if (!isAuthor) {
-//       return res.status(400).json("you are not the author");
-//     }
-//     await postDoc.update({
-//       title,
-//       summary,
-//       content,
-//       cover: newPath ? newPath : postDoc.cover,
-//     });
-
-//     res.json(postDoc);
-//   });
-// });
 
 app.get("/post", async (req, res) => {
   res.json(
